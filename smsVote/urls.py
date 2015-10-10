8555840cff2b9ef9c0ec63ffac063e0cf6e75fbe
Sasha Azad <sazad6@gatech.edu>
2015-10-10 15:22:23 -0400

    Changed URL Endpoints to add GET calls

--- smsVote/urls.py
+++ smsVote/urls.py
@@ -11,8 +11,10 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     
     url(r'^api/user/$', UserManager.userRequest, name='userrequest'),
+    url(r'^api/user/(?P<user_id>\d*)/$', UserManager.userRequest, name='userGet'),
     
     url(r'^api/booth/$', BoothManager.boothRequest, name='boothrequest'),
+    url(r'^api/booth/(?P<booth_id>\d*)/$', BoothManager.boothRequest, name='boothGet'),
     
     url(r'^api/booth/sms/$', BoothManager.boothSMS, name='boothrequest')
 )
